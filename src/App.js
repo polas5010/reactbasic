2bae93f3ac63ddb277ca92fb7c5a4a74a342c7d8
Parvez Hasan <polas.p5010@gmail.com>
2024-01-05 17:20:10 +0600

    navigation working

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ import Contact from "./page/Contact";
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 }
